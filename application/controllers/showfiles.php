bb532b099ce21332989ce40d7aa8169d3f9d5405
Uncater <leonid.neugodnikov@gmail.com>
2015-12-28 11:26:30 +0200

    Update showfiles.php

--- application/controllers/showfiles.php
+++ application/controllers/showfiles.php
@@ -12,12 +12,14 @@ class showFiles extends CI_Controller
             $id = $this->ion_auth->user()->row()->id;
             $query = $this->db->get_where('files', array('user_id' => $id));
 //            var_dump($query);
+            if ($query->num_rows() > 0){
             foreach($query->result() as $row){
                 $result[] =  $row->path;
             }
             $data['result'] = $result;
 //            var_dump($result);
-            $this->load->view('files', $data);
+            $this->load->view('files', $data);}
+            else echo "У вас еще нет документов"
 //            var_dump($query);
 
         }
